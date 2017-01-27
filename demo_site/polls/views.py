bc818924677ef0ebd353b0343bf6697b23f31a36
debugger2017 <kartik.p.satoskar@gmail.com>
2017-01-27 17:44:16 +0530

    Added harcoded view for index

--- demo_site/polls/views.py
+++ demo_site/polls/views.py
@@ -1,10 +1,9 @@
 from django.http import HttpResponse
 
+from .models import Question
+
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
-    
-def index(request):
-	return (HttpResponse("Hello,You are at polls index"))
 
 def results(request,question_id):
 	response = "You are looking at the results of question %s."
@@ -13,3 +12,7 @@ def results(request,question_id):
 def vote(request,question_id):
 	return HttpResponse("You are voting on question %s" % question_id)
 
+def index(request):
+	latest_question_list = Question.objects.order_by('-pub_date')[:5]
+	output = ', '.join([q.question_text for q in latest_question_list])
+	return HttpResponse(output)		
