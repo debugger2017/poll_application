1bf35077cc896194ed4d049867f03a6c95a5ca7c
debugger2017 <kartik.p.satoskar@gmail.com>
2017-01-27 17:09:14 +0530

    Added view for result and vote

--- demo_site/polls/views.py
+++ demo_site/polls/views.py
@@ -2,3 +2,11 @@ from django.http import HttpResponse
 
 def index(request):
 	return (HttpResponse("Hello,You are at polls index"))
+
+def results(request,question_id):
+	response = "You are looking at the results of question %s."
+	return HttpResponse(response % question_id)
+
+def vote(request,question_id):
+	return HttpResponse("You are voting on question %s" % question_id)
+	 
