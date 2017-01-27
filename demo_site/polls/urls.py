7b5ff5dea72492782dc537bc6181474dc85a0e37
debugger2017 <kartik.p.satoskar@gmail.com>
2017-01-27 17:35:36 +0530

    Removed syntax error

--- demo_site/polls/urls.py
+++ demo_site/polls/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-	url(r'^(?<question_id>[0-9]+)/$' , views.detail,name = 'detail'),
-	url(r'^(?<question_id>[0-9]+)/results/$' , views.results,name = 'results'),
-	url(r'^(?<question_id>[0-9]+)/vote/$' , views.vote,name = 'vote'),
+	url(r'^(?P<question_id>[0-9]+)/$', views.detail,name = 'detail'),
+	url(r'^(?P<question_id>[0-9]+)/results/$' , views.results,name = 'results'),
+	url(r'^(?P<question_id>[0-9]+)/vote/$' , views.vote,name = 'vote'),
 
 ]
