5e0fb7f5dd2259f610c592bc0d082f049468a735
debugger2017 <kartik.p.satoskar@gmail.com>
2017-01-28 16:47:35 +0530

    Added namespacing

--- demo_site/polls/urls.py
+++ demo_site/polls/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import url
-from django.template import loader
 
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
 	url(r'^(?P<question_id>[0-9]+)/$', views.detail,name = 'detail'),
