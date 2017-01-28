bf74af8613eb99e43b089c9ac3e292cdb8189a6a
debugger2017 <kartik.p.satoskar@gmail.com>
2017-01-28 16:27:46 +0530

    Added get module

--- demo_site/polls/views.py
+++ demo_site/polls/views.py
@@ -1,5 +1,5 @@
 from .models import Question
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.http import Http404
 
 def detail(request, question_id):
